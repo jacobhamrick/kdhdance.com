5d7308f04606eb3722a4ac79476cba56c63b24b2
Jacob Hamrick <thejacobhamrick@gmail.com>
2019-09-29 22:00:45 -0400

    Continued Class Page style edits.

--- page-classes.php
+++ page-classes.php
@@ -8,26 +8,41 @@
 <main>
   <div class="main-body scroll">
     <div class="page-body">
-      <h2 id="upcoming-season"><?php wp_title('', true,''); ?></h2>
+      <h2 id="classes-title"><?php wp_title('', true,''); ?></h2>
+      <p id="classes-intro"><?php the_field('classes-intro'); ?>
       <div class="classes-wrapper flex">
-        <!-- show 1 -->
+        <!-- class 1 -->
         <div class="class-container">
-          <p class="show-title"><?php the_field('class-day-1'); ?></p>
-          <p class="show-date"><?php the_field('class-time-1'); ?></p>
-          <p class="show-info"><?php the_field('class-info-1'); ?></p>
+          <p class="class-day"><?php the_field('class-day-1'); ?></p>
+          <p class="class-time"><?php the_field('class-time-1'); ?></p>
+          <p class="class-info"><?php the_field('class-info-1'); ?></p>
         </div>
-        <!-- show 2 -->
+        <!-- class 2 -->
         <div class="class-container">
-          <p class="show-title"><?php the_field('class-day-2'); ?></p>
-          <p class="show-date"><?php the_field('class-time-2'); ?></p>
-          <p class="show-info"><?php the_field('class-info-2'); ?></p>
+          <p class="class-day"><?php the_field('class-day-2'); ?></p>
+          <p class="class-time"><?php the_field('class-time-2'); ?></p>
+          <p class="class-info"><?php the_field('class-info-2'); ?></p>
         </div>
-        <!-- show 3 -->
+        <!-- class 3 -->
         <div class="class-container">
-          <p class="show-title"><?php the_field('class-day-3'); ?></p>
-          <p class="show-date"><?php the_field('class-time-3'); ?></p>
-          <p class="show-info"><?php the_field('class-info-3'); ?></p>
+          <p class="class-day"><?php the_field('class-day-3'); ?></p>
+          <p class="class-time"><?php the_field('class-time-3'); ?></p>
+          <p class="class-info"><?php the_field('class-info-3'); ?></p>
         </div>
+        <!-- class 4 -->
+        <div class="class-container">
+          <p class="class-day"><?php the_field('class-day-4'); ?></p>
+          <p class="class-time"><?php the_field('class-time-4'); ?></p>
+          <p class="class-info"><?php the_field('class-info-4'); ?></p>
+        </div>
+        <!-- class feature -->
+        <div id="class-feature-container">
+          <p><?php the_field('class-feature'); ?></p>
+          <?php if( get_field('class-feature-photo') ): ?>
+              <img src="<?php the_field('class-feature-photo'); ?>" />
+          <?php endif; ?>
+        </div>
+
       </div>
       <!-- end upcoming-wrapper -->
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
