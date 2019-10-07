1530da887b1b2782480b64b047f0fd8ebcd415d1
Jacob Hamrick <thejacobhamrick@gmail.com>
2019-10-06 22:31:05 -0400

    Final phase style build for Classes and Upcoming.

--- page-classes.php
+++ page-classes.php
@@ -11,46 +11,47 @@
       <h2 id="classes-title"><?php wp_title('', true,''); ?></h2>
       <p id="classes-intro"><?php the_field('classes-intro'); ?></p>
       <div class="classes-wrapper flex">
+
         <!-- class 1 -->
         <div class="class-container">
           <p class="class-day"><?php the_field('class-day-1'); ?></p>
           <p class="class-time"><?php the_field('class-time-1'); ?></p>
-          <span class="class-info"><?php the_field('class-info-1'); ?></span>
+          <?php echo '<div class="class-info">'; the_field('class-info-1'); echo '</div>';?>
         </div>
         <!-- class 2 -->
         <div class="class-container">
           <p class="class-day"><?php the_field('class-day-2'); ?></p>
           <p class="class-time"><?php the_field('class-time-2'); ?></p>
-          <span class="class-info"><?php the_field('class-info-2'); ?></span>
+          <?php echo '<div class="class-info">'; the_field('class-info-2'); echo '</div>';?>
         </div>
         <!-- class 3 -->
         <div class="class-container">
           <p class="class-day"><?php the_field('class-day-3'); ?></p>
           <p class="class-time"><?php the_field('class-time-3'); ?></p>
-          <span class="class-info"><?php the_field('class-info-3'); ?></span>
+          <?php echo '<div class="class-info">'; the_field('class-info-3'); echo '</div>';?>
         </div>
         <!-- class 4 -->
         <div class="class-container">
           <p class="class-day"><?php the_field('class-day-4'); ?></p>
           <p class="class-time"><?php the_field('class-time-4'); ?></p>
-          <p class="class-info"><?php the_field('class-info-4'); ?></p>
+          <?php echo '<div class="class-info">'; the_field('class-info-4'); echo '</div>';?>
         </div>
-        <!-- class feature -->
-      </div>
-      <!-- end classes-wrapper -->
+
+      </div> <!-- end classes-wrapper -->
+
+      <!-- class feature -->
       <div id="class-feature-container">
         <p id="class-feature-title"><?php the_field('class-feature-title'); ?></p>
-        <span id="class-feature"><?php the_field('class-feature'); ?>
+        <div id="class-feature"><?php the_field('class-feature'); ?>
           <?php if( get_field('class-feature-photo') ): ?>
-              <img src="<?php the_field('class-feature-photo'); ?>" />
-        </span>
+              <img id="class-feature-photo" src="<?php the_field('class-feature-photo'); ?>" />
+        </div>
           <?php endif; ?>
       </div>
         <!--
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <?php the_content(); ?> -->
-    </div>
-    <!-- end page-body -->
+    </div> <!-- end page-body -->
       <?php endwhile; endif; ?>
   </div>
 </main>
