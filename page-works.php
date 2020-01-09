a6331ccf81191535fc41827371031b73e1c121bd
Jacob Hamrick <thejacobhamrick@gmail.com>
2020-01-09 00:35:21 -0500

    Portfolio build progress.

--- page-works.php
+++ page-works.php
@@ -3,21 +3,34 @@
   Template Name: Page Works
 */
 ?>
+
 <?php get_header(); ?>
 <main>
-  <div class="main-body scroll">
+
+  <nav id="portfolio-nav" class="flex">
+    <ul class="flex">
+      <a href="#piece-title-1"><li><?php the_field('piece-title-1'); ?></li></a>
+    </ul>
+  </nav>
+
+  <div id="portfolio-main-body" class="main-body scroll">
+
     <div class="page-body <?php wp_title('', true,''); echo '-div'?>">
-      <h2 class="page-title"><?php the_title(); ?></h2>
-      <div class="page-content">
-      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <?php the_content(); ?>
+
+
+      <div id="portfolio">
+
+        <div class="portfolio-piece">
+          <p id="piece-title-1" class="piece-title"><?php the_field('piece-title-1'); ?></p>
+
+          <?php echo '<div class="piece-text">'; the_field('piece-text-1'); echo '</div>';?>
+        </div>
+        <!-- end portfolio-piece -->
+
       </div>
+
     </div>
     <!-- end page-body -->
-
-    <?php endwhile; else : ?>
-    	<p><?php esc_html_e( 'Sorry, no pages found.' ); ?></p>
-      <?php endif; ?>
   </div>
     <!-- end main-body-scroll -->
 </main>
